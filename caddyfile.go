91f2291d3144497b45fce00126bd8e384eca5bcc
bobby4k <bobby4kit@outlook.com>
2024-08-16 22:33:57 +0800

    test v1

--- caddyfile.go
+++ caddyfile.go
@@ -1,9 +1,25 @@
 package redislogger
 
 import (
+	"github.com/caddyserver/caddy/v2"
 	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
+	"github.com/caddyserver/caddy/v2/caddyconfig/httpcaddyfile"
+	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
 )
 
+func init() {
+	caddy.RegisterModule(RedisLogger{})
+	httpcaddyfile.RegisterHandlerDirective("redislogger", parseCaddyfile)
+}
+
+func (RedisLogger) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		ID:  "http.handlers.redis_logger",
+		New: func() caddy.Module { return new(RedisLogger) },
+	}
+}
+
+// UnmarshalCaddyfile实现了caddyfile.Unmarshaler
 func (rl *RedisLogger) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
 	for d.Next() {
 		if !d.Args(&rl.RedisKey) {
@@ -27,3 +43,18 @@ func (rl *RedisLogger) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
 	}
 	return nil
 }
+
+// parseCaddyfile从h中解读令牌到一个新的中间件。
+func parseCaddyfile(h httpcaddyfile.Helper) (caddyhttp.MiddlewareHandler, error) {
+	var rl RedisLogger
+	err := rl.UnmarshalCaddyfile(h.Dispenser)
+	return &rl, err
+}
+
+// Interface guards
+var (
+	_ caddy.Provisioner           = (*RedisLogger)(nil)
+	_ caddy.Validator             = (*RedisLogger)(nil)
+	_ caddyhttp.MiddlewareHandler = (*RedisLogger)(nil)
+	_ caddyfile.Unmarshaler       = (*RedisLogger)(nil)
+)
--- redis_logger.go
+++ redis_logger.go
@@ -3,8 +3,10 @@ package redislogger
 import (
 	"context"
 	"encoding/json"
-	"io/ioutil"
+	"fmt"
+	"io"
 	"net/http"
+	"time"
 
 	"github.com/caddyserver/caddy/v2"
 	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
@@ -12,53 +14,108 @@ import (
 	"go.uber.org/zap"
 )
 
-func init() {
-	caddy.RegisterModule(RedisLogger{})
-}
-
 type RedisLogger struct {
-	RedisAddress  string `json:"redis_address"`
-	RedisPassword string `json:"redis_password"`
-	RedisKey      string `json:"redis_key"`
-	WithBody      bool   `json:"with_body,omitempty"`
+	RedisAddress  string        `json:"redis_address,omitempty"`
+	RedisPassword string        `json:"redis_password,omitempty"`
+	RedisDB       int           `json:"redis_db,omitempty"`
+	RedisKey      string        `json:"redis_key"`
+	WithBody      bool          `json:"with_body,omitempty"`
+	DialTimeout   time.Duration `json:"dial_timeout,omitempty"`  // 连接超时时间
+	ReadTimeout   time.Duration `json:"read_timeout,omitempty"`  // 读取超时时间
+	WriteTimeout  time.Duration `json:"write_timeout,omitempty"` // 写入超时时间
+	MaxRetries    int           `json:"max_retries,omitempty"`   // 最大重试次数
 	client        *redis.Client
 	logger        *zap.Logger
 }
 
-func (RedisLogger) CaddyModule() caddy.ModuleInfo {
-	return caddy.ModuleInfo{
-		ID:  "http.handlers.redis_logger",
-		New: func() caddy.Module { return new(RedisLogger) },
-	}
-}
-
+// Provision实现了caddy.Provisioner
 func (rl *RedisLogger) Provision(ctx caddy.Context) error {
 	rl.logger = ctx.Logger(rl)
 
+	// 设置默认配置
+	if rl.RedisAddress == "" {
+		rl.RedisAddress = "localhost:6379"
+	}
+	if rl.RedisDB == 0 {
+		rl.RedisDB = 0
+	}
+	if rl.DialTimeout == 0 {
+		rl.DialTimeout = 5 * time.Second // 默认连接超时时间
+	}
+	if rl.ReadTimeout == 0 {
+		rl.ReadTimeout = 3 * time.Second // 默认读取超时时间
+	}
+	if rl.WriteTimeout == 0 {
+		rl.WriteTimeout = 3 * time.Second // 默认写入超时时间
+	}
+	if rl.MaxRetries == 0 {
+		rl.MaxRetries = 3 // 默认最大重试次数
+	}
+
 	rl.client = redis.NewClient(&redis.Options{
-		Addr:     rl.RedisAddress,
-		Password: rl.RedisPassword,
+		Addr:         rl.RedisAddress,
+		Password:     rl.RedisPassword,
+		DB:           rl.RedisDB,
+		DialTimeout:  rl.DialTimeout,
+		ReadTimeout:  rl.ReadTimeout,
+		WriteTimeout: rl.WriteTimeout,
+		MaxRetries:   rl.MaxRetries,
 	})
 
 	return nil
 }
 
+// Validate实现了caddy.Validator
+func (rl *RedisLogger) Validate() error {
+	if rl.client == nil {
+		return fmt.Errorf("no redis connet")
+	}
+	return nil
+}
+
+// ServeHTTP 实现了 caddyhttp.MiddlewareHandler
 func (rl *RedisLogger) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
+	start := time.Now()
 	recorder := caddyhttp.NewResponseRecorder(w, nil, nil)
 
 	if err := next.ServeHTTP(recorder, r); err != nil {
 		return err
 	}
 
+	duration := time.Since(start).Seconds()
 	logEntry := map[string]interface{}{
-		"method":      r.Method,
-		"uri":         r.RequestURI,
-		"status":      recorder.Status(),
-		"remote_addr": r.RemoteAddr,
+		// "level":  "info",
+		"ts": time.Now().Format(time.RFC3339Nano),
+		// "logger": "http.log.access.log0",
+		// "msg":    "handled request",
+		"request": map[string]interface{}{
+			"remote_ip":   r.RemoteAddr,
+			"remote_port": r.URL.Port(),
+			"client_ip":   r.Header.Get("X-Forwarded-For"),
+			"proto":       r.Proto,
+			"method":      r.Method,
+			"host":        r.Host,
+			"uri":         r.RequestURI,
+			"headers":     r.Header,
+			"tls": map[string]interface{}{
+				"resumed":      r.TLS.DidResume,
+				"version":      r.TLS.Version,
+				"cipher_suite": r.TLS.CipherSuite,
+				"proto":        r.TLS.NegotiatedProtocol,
+				"server_name":  r.TLS.ServerName,
+			},
+		},
+		"bytes_read": r.ContentLength,
+		// "user_id":      "", // 可以根据需求设置用户ID
+		"duration":     duration,
+		"size":         recorder.Size(),
+		"status":       recorder.Status(),
+		"resp_headers": recorder.Header(),
 	}
 
 	if rl.WithBody {
-		body, err := ioutil.ReadAll(r.Body)
+		// https://github.com/caddyserver/caddy/commit/6f0f159ba56adeb6e2cbbb408651419b87f20856
+		body, err := io.ReadAll(r.Body)
 		if err != nil {
 			rl.logger.Error("Error reading request body", zap.Error(err))
 			return err
